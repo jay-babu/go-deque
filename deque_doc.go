79315170085f5cbd44b4c5bb9748df07a1d9be53
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-07-05 19:25:20 -0400

    adding benchmark

--- deque_doc.go
+++ deque_doc.go
@@ -7,7 +7,18 @@ The internal nodes are abstracted from the user and the stored value is only exp
 
 Benchmarking
 
-To be added.
+Benchmark Tests that do not contain a suffix use the Deque.
+Tests ending in `_List` use the standard slice.
+```
+BenchmarkAppendFirst-16                 20438316                65.57 ns/op           24 B/op          1 allocs/op
+BenchmarkAppendFirst_List-16            100000000               30.27 ns/op           45 B/op          0 allocs/op
+
+BenchmarkAppendPopFirst-16              17881148                78.14 ns/op           24 B/op          1 allocs/op
+BenchmarkAppendPopFirst_List-16         100000000               32.96 ns/op           45 B/op          0 allocs/op
+
+BenchmarkInsertAt-16                    20175003                66.46 ns/op           24 B/op          1 allocs/op
+BenchmarkInsertAt_List-16               100000000               34.19 ns/op           45 B/op          0 allocs/op
+```
 
 Generics
 
