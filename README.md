1361abb5063d1332515e55f13fa04b594481c903
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-07-15 21:58:48 -0400

    updating docs

--- README.md
+++ README.md
@@ -3,16 +3,6 @@
 [![Go Reference](https://pkg.go.dev/badge/github.com/jayp0521/go-deque.svg)](https://pkg.go.dev/github.com/jayp0521/go-deque)
 [![Go Report Card](https://goreportcard.com/badge/github.com/jayp0521/go-deque)](https://goreportcard.com/report/github.com/jayp0521/go-deque)
 
-# Benchmark
-
-```
-BenchmarkAppendFirst-16                 20438316                65.57 ns/op           24 B/op          1 allocs/op
-BenchmarkAppendFirst_List-16            100000000               30.27 ns/op           45 B/op          0 allocs/op
-
-BenchmarkAppendPopFirst-16              17881148                78.14 ns/op           24 B/op          1 allocs/op
-BenchmarkAppendPopFirst_List-16         100000000               32.96 ns/op           45 B/op          0 allocs/op
-
-BenchmarkInsertAt-16                    20175003                66.46 ns/op           24 B/op          1 allocs/op
-BenchmarkInsertAt_List-16               100000000               34.19 ns/op           45 B/op          0 allocs/op
-```
+# Docs
+https://pkg.go.dev/github.com/jayp0521/go-deque#section-documentation
 
--- deque_doc.go
+++ deque_doc.go
@@ -1,33 +1,22 @@
 /*
 Package deque provides a fast double ended queue implementation.
 
-Deque allows the addition & removal of items at either of the queue
-in O(1) perfomance. FIFO and LIFO operations are both supported.
+Deque allows the addition & removal of items at either of the queue. FIFO and LIFO operations are both supported.
 The internal nodes are abstracted from the user and the stored value is only exposed.
 
-Benchmarking
+This library is mostly a wrapper around a list with some logic abstracted away.
+If an operation would throw an error on a list, it will be thrown here.
 
-Benchmark Tests that do not contain a suffix use the Deque.
-Tests ending in `_List` use the standard slice.
-```
-BenchmarkAppendFirst-16                 20438316                65.57 ns/op           24 B/op          1 allocs/op
-BenchmarkAppendFirst_List-16            100000000               30.27 ns/op           45 B/op          0 allocs/op
+> Implementation using a traditional deque was done with O(1) operations
+at the beginning and end. Using a list turned out to be more performant.
 
-BenchmarkAppendPopFirst-16              17881148                78.14 ns/op           24 B/op          1 allocs/op
-BenchmarkAppendPopFirst_List-16         100000000               32.96 ns/op           45 B/op          0 allocs/op
+Benchmark
 
-BenchmarkInsertAt-16                    20175003                66.46 ns/op           24 B/op          1 allocs/op
-BenchmarkInsertAt_List-16               100000000               34.19 ns/op           45 B/op          0 allocs/op
-```
+To be added.
 
 Generics
 
 This deque is allowed to be made up of any value the user specifies.
 
-Empty Deque
-
-This queue implementation returns a boolean as a second value in certain operations.
-True means the first value returned is part of the queue.
-False means the first value returned did not exist. The value returned is the zeroth value of the type.
 */
 package deque
